753ae5e56715a62d54505173a757bfb60266ce9f
aheed <anders.heed@combination.se>
2021-12-03 22:26:43 +0100

    cleanup

--- adv3_2.py
+++ adv3_2.py
@@ -1,11 +1,6 @@
 import utils
 
-lines = utils.get_in_file()
-
 ints = utils.get_ints_from_in_file_lines(2)
-
-oxy = 0
-co2 = 0
 
 def oxy_criterion(inarr, pos) -> bool:
     sum = 0
@@ -22,38 +17,23 @@ def oxy_number_ok(inarr, pos, number) -> bool:
     bit_val = (number >> pos) % 2
     return oxy_criterion(inarr, pos) == bit_val
 
-def co2_number_ok(inarr, pos, number) -> bool:
-    bit_val = (number >> pos) % 2
-    return (not oxy_criterion(inarr, pos)) == bit_val
-
-
-def oxy_criterion_at_pos(inarr, pos):
+def criterion_at_pos(inarr, pos, co2):
     def f(number):
-        return oxy_number_ok(inarr, pos, number)
+        return co2 ^ oxy_number_ok(inarr, pos, number)
     return f
 
-def co2_criterion_at_pos(inarr, pos):
-    def f(number):
-        return co2_number_ok(inarr, pos, number)
-    return f
+def get_rate(co2) -> int:
+    pos = 11
+    modarr = ints
+    while(len(modarr) > 1):
+        modarr = list(filter(criterion_at_pos(modarr, pos, co2), modarr))
+        pos -= 1
+    return modarr[0]    
 
+oxy_res = get_rate(False)
+#print("oxy", oxy_res)
 
-pos = 11
-modarr = ints
-while(len(modarr) > 1):
-    modarr = list(filter(oxy_criterion_at_pos(modarr, pos), modarr))
-    pos -= 1
-    #print(len(modarr))
-oxy = modarr[0]
-#print("oxy", oxy)
+co2_res = get_rate(True)
+#print("co2", co2_res)
 
-pos = 11
-modarr = ints
-while(len(modarr) > 1):
-    modarr = list(filter(co2_criterion_at_pos(modarr, pos), modarr))
-    pos -= 1
-    #print(len(modarr))
-co2 = modarr[0]
-#print("co2", co2)
-
-print(oxy * co2)
+print(oxy_res * co2_res)
