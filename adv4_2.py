aa9d575e98fc8f5a9c04dc10930b55f8edd783a2
aheed <anders.heed@combination.se>
2021-12-05 12:15:37 +0100

    day 4 part 2 completed

---
+++ adv4_2.py
@@ -0,0 +1,105 @@
+from typing import List
+import utils
+
+infile = utils.get_in_file()
+lines = [line for line in infile]
+infile.close()
+
+#print(lines)
+
+drawn_numbers = [int(num) for num in lines[0].split(',')]
+#print(drawn_numbers)
+
+infile = utils.get_in_file()
+all_boards_texts = infile.read().split('\n\n')[1:]
+
+
+class BingoNumber():
+    drawn: bool
+    def __init__(self, numval: int):
+        self.numval = numval
+        self.drawn = False
+    
+    def __str__(self):
+        return f'{self.numval} {self.drawn}'
+
+    def __repr__(self):
+        return self.__str__()
+
+class Board():
+    last_drawn: int
+
+    def __init__(self, fives: list[list[BingoNumber]]):
+        self.fives = fives
+        
+    def draw_number(self, number: int):
+        for row in self.fives:
+            for bingo_num in row:
+                if (bingo_num.numval == number):
+                    bingo_num.drawn = True
+                    self.last_drawn = number
+
+    def got_bingo(self) -> bool:
+        for col in range(5):
+            all_drawn = True
+            for row in range(5):
+                all_drawn = all_drawn and self.fives[row][col].drawn
+            if (all_drawn):
+                return True
+
+        for row in range(5):
+            all_drawn = True
+            for col in range(5):
+                all_drawn = all_drawn and self.fives[row][col].drawn
+            if (all_drawn):
+                return True
+
+        return False
+
+    def calc_score(self):
+        sum = 0
+        for row in range(5):
+            for col in range(5):
+                if (not self.fives[row][col].drawn):
+                    sum += self.fives[row][col].numval
+        return sum * self.last_drawn
+
+def create_row(text: str) -> list[BingoNumber]:
+    numbers = text.strip().split(' ')
+    return [BingoNumber(int(num)) for num in numbers if len(num) > 0]
+
+def create_rows(text: str) -> list[list[BingoNumber]]:
+    return [create_row(five_str) for five_str in text.split('\n')]
+
+
+def create_board(text: str):
+    rows = create_rows(text)
+    #cols = create_cols(text)
+    return Board(rows)
+
+boards = [create_board(board_text) for board_text in all_boards_texts]
+
+def get_first_bingo_board():
+    for drawn_num in drawn_numbers:
+        board_index = 0
+        for board in boards:
+            board.draw_number(drawn_num)
+            if (board.got_bingo()):
+                return board_index
+            board_index += 1
+
+def get_last_bingo_board() -> Board:
+    boards_with_bingo: list[Board] = []
+    for drawn_num in drawn_numbers:
+        board_index = 0
+        for board in boards:
+            if (not board.got_bingo()):
+                board.draw_number(drawn_num)
+                if (board.got_bingo()):
+                    boards_with_bingo.append(board)
+            board_index += 1
+    return boards_with_bingo[-1]
+
+worst_board = get_last_bingo_board()
+
+print(worst_board.calc_score())
