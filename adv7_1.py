7c4099047438fb4cb9d685fc795d1d659a4db54c
aheed <anders.heed@combination.se>
2021-12-07 19:13:06 +0100

    cleanup

--- adv7_1.py
+++ adv7_1.py
@@ -1,31 +1,18 @@
 import utils
-from functools import cache
 
 with utils.get_in_file() as infile:
     lines = [line for line in infile]
 
 crabs = [int(pos) for pos in lines[0].split(",")]
 
-print(crabs)
+def calc_move_cost(origin: int, target_position: int) -> int:
+    return abs(origin - target_position)
 
-
-average = round(sum(crabs) / len(crabs))
-print(average)
-
-def calc_cost(positions: list[int], target_position: int):
-    costs = [abs(origin - target_position) for origin in positions]
+def calc_cost(positions: list[int], target_position: int) -> int:
+    costs = [calc_move_cost(origin, target_position) for origin in positions]
     return sum(costs)
 
-
-res = calc_cost(crabs, average)
-print(res)
-
-#search_window = 20
-#results = [calc_cost(crabs, candidate) for candidate in range(average - search_window, average + search_window) ]
 results = [calc_cost(crabs, candidate) for candidate in range(len(crabs)) ]
-
-print(results)
-print(len(results))
 
 min_cost = min(results)
 print(min_cost)
--- adv7_2.py
+++ adv7_2.py
@@ -1,35 +1,21 @@
 import utils
-from functools import cache
 
 with utils.get_in_file() as infile:
     lines = [line for line in infile]
 
 crabs = [int(pos) for pos in lines[0].split(",")]
 
-print(crabs)
-
-
-average = round(sum(crabs) / len(crabs))
-print(average)
-
-def calc_move_cost(origin: int, target_position: int):
+def calc_move_cost(origin: int, target_position: int) -> int:
     distance = abs(origin - target_position)
-    cost = sum([i for i in range(distance + 1)]) #naive!!
-    return cost
+    #cost = sum([i for i in range(distance + 1)]) #naive!!
+    cost = (distance + 1) * (distance / 2)
+    return int(cost)
 
-
-def calc_cost(positions: list[int], target_position: int):
+def calc_cost(positions: list[int], target_position: int) -> int:
     costs = [calc_move_cost(origin, target_position) for origin in positions]
     return sum(costs)
 
-
-res = calc_cost(crabs, average)
-print(res)
-
-#search_window = 20
-#results = [calc_cost(crabs, candidate) for candidate in range(average - search_window, average + search_window) ]
 results = [calc_cost(crabs, candidate) for candidate in range(len(crabs)) ]
-print(results)
 
 min_cost = min(results)
 print(min_cost)
