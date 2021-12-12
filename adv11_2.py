f1179150970033d0650423ba2a84f3b52f2a9469
aheed <anders.heed@combination.se>
2021-12-12 13:42:18 +0100

    day 11 part 2 completed

---
+++ adv11_2.py
@@ -0,0 +1,72 @@
+import utils
+
+
+with utils.get_in_file() as infile:
+    lines = [line.strip() for line in infile]
+
+energy: list[list[int]] = [[int(depth) for depth in list(line.strip())] for line in lines]
+size = len(energy[0])
+total_flashes = int(0)
+flashes_last_step = int(0)
+
+def is_in_bounds(row: int, col: int) -> bool:
+    return row >= 0 and row < size and col >= 0 and col < size
+
+def get_neighbors(row: int, col: int) -> list[list[int]]:
+    neigh: list[list[int]] = []
+
+    def add_if_in_bounds(row: int, col: int):
+        if is_in_bounds(row, col):
+            neigh.append([row, col])
+    
+    add_if_in_bounds(row - 1, col)
+    add_if_in_bounds(row + 1, col)
+    add_if_in_bounds(row, col - 1)
+    add_if_in_bounds(row, col + 1)
+    add_if_in_bounds(row - 1, col - 1)
+    add_if_in_bounds(row + 1, col - 1)
+    add_if_in_bounds(row - 1, col + 1)
+    add_if_in_bounds(row + 1, col + 1)
+    return neigh
+
+def calc_energy_step():
+
+    global energy
+    global total_flashes
+    global flashes_last_step
+
+    flashes_last_step = 0
+
+    flashed: list[list[int]] = []
+    for _ in range(size):
+        flashed.append([False] * size)
+    
+    for row in range(size):
+            for col in range(size):
+                energy[row][col] += 1
+
+    calc_pending = True
+
+    while (calc_pending):
+        calc_pending = False
+        for row in range(size):
+            for col in range(size):
+                if (energy[row][col] > 9 and not flashed[row][col]):
+                    flashed[row][col]= True
+                    total_flashes += 1
+                    flashes_last_step += 1
+                    calc_pending = True
+                    for neigh in get_neighbors(row, col):
+                        energy[neigh[0]][neigh[1]] += 1
+
+    for row in range(size):
+        for col in range(size):
+            if flashed[row][col]:
+                energy[row][col] =0
+
+for i in range(100000):
+    calc_energy_step()
+    if (flashes_last_step == (size * size)):
+        print(i + 1)
+        break
+
